2fa717a7df4e80f95a28068f6dcbec45e0a7e1ec
HyperMakson <maks03work@gmail.com>
2024-04-26 23:21:28 +0300

    main-page test

--- index.php
+++ index.php
@@ -11,7 +11,12 @@
 <body>
     <div class="header">
         <div class="header__navbar">
-            <div class="navbar__logo"></div>
+            <div class="navbar__fast-link">
+                <p>Ачивки</p>
+            </div>
+            <div class="navbar__logo">
+                <p>LOGO</p>
+            </div>
             <div class="navbar__link">
                 <div class="link__log">
                     <a href="">Вход</a>
@@ -23,13 +28,30 @@
         </div>
     </div>
     <div class="main">
-        <div class="main-container">
-            <p>Test</p>
-            <p>hHFHHFHFHFHFH</p>
+        <div class="main-logo">
+            <div class="main-container">
+                <div class="main-text__container">
+                    <p class="main-text">Личный кабинет туриста</p>
+                </div>
+            </div>
+            <div class="history-travel">
+                <p>Здесь поиск</p>
+            </div>
+        </div>
+        <div class="main-content">
+            <div class="block-news block-news__margin">
+                <p>Здесь выводит туры</p>
+            </div>
+            <div class="block-news">
+                <p>Здесь что-нибудь выводить</p>
+            </div>
+            <div class="block-news">
+                <p>Здесь отзывы</p>
+            </div>
         </div>
     </div>
     <div class="footer">
-
+        <p>FOOTER</p>
     </div>
 </body>
 
