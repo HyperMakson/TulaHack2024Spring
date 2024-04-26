46ebbe23868e7070b84af7da96c3f68a416c2373
HyperMakson <maks03work@gmail.com>
2024-04-27 01:19:56 +0300

    fix headfoot

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/style.css">
-    <title>TulaHack</title>
-</head>
+<?
+require_once $_SERVER["DOCUMENT_ROOT"] . '/include_headfoot/header.php';
+?>
 
 <body>
     <div class="header">
@@ -50,9 +44,9 @@
             </div>
         </div>
     </div>
-    <div class="footer">
-        <p>FOOTER</p>
-    </div>
+    <?
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/include_headfoot/footer.php';
+    ?>
     <script src="scripts/script.js"></script>
 </body>
 
