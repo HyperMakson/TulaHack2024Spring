a05fcc6d9c648f102b156575917da43f4165dd76
HyperMakson <maks03work@gmail.com>
2024-04-27 13:10:03 +0300

    добавление слайдера

--- logreg/script_logreg.js
+++ logreg/script_logreg.js
@@ -3,3 +3,10 @@ var offsetHeightLabelLog = document.querySelectorAll('.upper-text__log-form');
 offsetHeightLabelLog.forEach(el => {
     el.style.top = "-" + ((el.offsetHeight + 4) / 2) + "px";
 });
+
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
